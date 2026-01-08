00476b2fadd7a32511395b01d1145df695f9c1a4
Grzegorz Daszuta <grzegorz.daszuta@codewave.pl>
2026-01-08 09:13:11 +0100

    Add safe fallback for missing attributes in graphql logger

--- src/Hooks/GraphQlQueryLoggerHook.php
+++ src/Hooks/GraphQlQueryLoggerHook.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Codewave\OpenTelemetry\Magento\Hooks;
 
-use Magento\Framework\App\Action\Action;
+use Magento\GraphQl\Model\Query\Logger\LoggerInterface;
 use Magento\GraphQl\Model\Query\Logger\LoggerPool;
-use OpenTelemetry\Context\Context;
 use OpenTelemetry\API\Trace\Span;
 use OpenTelemetry\SemConv\TraceAttributes;
-use Throwable;
 
 use function OpenTelemetry\Instrumentation\hook;
 
@@ -31,10 +29,10 @@ class GraphQlQueryLoggerHook
                 $parentSpan = Span::getCurrent();
                 if ($parentSpan->isRecording() && $parentSpan->getName() === GraphQlControllerHook::SPAN_NAME) {
                     $parentSpan
-                        ->updateName(sprintf('GraphQl %s', $queryDetails['GraphQlTopLevelOperationName']))
-                        ->setAttribute(TraceAttributes::GRAPHQL_OPERATION_NAME, $queryDetails['GraphQlTopLevelOperationName'])
-                        ->setAttribute(self::GRAPHQL_OPERATION_NAMES, $queryDetails['GraphQlOperationNames'])
-                        ->setAttribute(self::GRAPHQL_OPERATION_NUMBER, $queryDetails['GraphQlNumberOfOperations']);
+                        ->updateName(sprintf('GraphQl %s', $queryDetails[LoggerInterface::TOP_LEVEL_OPERATION_NAME]))
+                        ->setAttribute(TraceAttributes::GRAPHQL_OPERATION_NAME, $queryDetails[LoggerInterface::TOP_LEVEL_OPERATION_NAME])
+                        ->setAttribute(self::GRAPHQL_OPERATION_NAMES, $queryDetails[LoggerInterface::OPERATION_NAMES] ?? "unknown")
+                        ->setAttribute(self::GRAPHQL_OPERATION_NUMBER, $queryDetails[LoggerInterface::NUMBER_OF_OPERATIONS] ?? "unknown");
                 }
 
                 return $params;
